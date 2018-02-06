ca31ff0f8c64685ced7e9003ae318c19014a4f29
ahmedelzaher1 <36195864+ahmedelzaher1@users.noreply.github.com>
2018-02-06 14:57:25 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,17 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login('MzU3NTM3ODc5ODYzNDU5ODQx.DJrbzg.7JUTLGGfH60BpFm2wBJULLIHCqg');
+client.on('ready', () => {
+    console.log('I am ready!');
+  {
+});
 
 client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
-
-  if (message.content === '/join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
