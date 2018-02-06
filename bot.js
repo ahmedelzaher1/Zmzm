c5166127f0f1130a9df9b29206091baf1a6c2f92
ahmedelzaher1 <36195864+ahmedelzaher1@users.noreply.github.com>
2018-02-06 15:51:46 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,26 @@ Client.on('message', message => {
   // If the message is "what is my avatar"
   if (message.content === 'what is my avatar') {
     // Send the user's avatar URL
-    message.reply(message.author.avatarURL);
+    message.channel.sendMessage(message.author.avatarURL);
+  }
+});
+
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === '/join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.channel.sendMessage('You need to join a voice channel first!');
+    }
   }
 });
 
