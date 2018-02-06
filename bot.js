382a8c9f5b349be1c0107f94765aeb6435306e7a
ahmedelzaher1 <36195864+ahmedelzaher1@users.noreply.github.com>
2018-02-06 06:11:30 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,22 @@
 const Discord = require(`discord.js`);
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
-bot.on(`message`,(message) => {
+client.on(`message`,(message) => {
     if (message.content == `Ahmed`) {
          message.channel.sendMessage(`The King ?`);
     }
+    
+    client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "what is my avatar"
+  if (message.content === 'what is my avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
+    
 });
 
 bot.login(`MzU3NTM3ODc5ODYzNDU5ODQx.DVsTXw.rxNUdEudQkAK5LSDUt2QRUbkZ2g`);
