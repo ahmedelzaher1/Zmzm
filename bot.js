f778cb8cf0f23f6a257ea026af7a4c49926a1f9e
ahmedelzaher1 <36195864+ahmedelzaher1@users.noreply.github.com>
2018-02-06 05:58:14 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ const Discord = require(`discord.js`);
 const bot = new Discord.Client();
 
 bot.on(`message`,(message) => {
-    if (message.content == `ping`) {
-         message.channel.sendMessage(`pong`);
+    if (message.content == `Ahmed`) {
+         message.channel.sendMessage(`The King ?`);
     }
 });
 
