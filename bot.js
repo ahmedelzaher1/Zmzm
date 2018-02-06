d69f32d2214184c99c0267d10abd1a93533f644c
ahmedelzaher1 <36195864+ahmedelzaher1@users.noreply.github.com>
2018-02-06 15:48:37 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,4 +7,13 @@ Client.on(`message`,(message) => {
     }
 });
 
+// Create an event listener for messages
+Client.on('message', message => {
+  // If the message is "what is my avatar"
+  if (message.content === 'what is my avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
+});
+
 Client.login(`MzU3NTM3ODc5ODYzNDU5ODQx.DVsTXw.rxNUdEudQkAK5LSDUt2QRUbkZ2g`);
