1b094ae138f116d38b4d093892d8f8e50be3a86b
ahmedelzaher1 <36195864+ahmedelzaher1@users.noreply.github.com>
2018-02-06 16:17:39 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,5 +15,22 @@ Client.on('message', message => {
     message.channel.sendMessage(message.author.avatarURL);
   }
 });
+
+// The ready event is vital, it means that your bot will only start reacting to information
+// from Discord _after_ ready is emitted
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
     
 Client.login(`MzU3NTM3ODc5ODYzNDU5ODQx.DVsTXw.rxNUdEudQkAK5LSDUt2QRUbkZ2g`);
